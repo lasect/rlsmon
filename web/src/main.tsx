3266503b84ec37dfa044f49271069a0acf8209a6
heywinit <heywinit@gmail.com>
2026-04-03 17:02:40 +0530

    style: update theme and styling

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { TRPCProvider } from "@/api/provider.tsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<TRPCProvider>
 		<ThemeProvider>
-			<App />
+			<TooltipProvider delayDuration={200}>
+				<App />
+			</TooltipProvider>
 		</ThemeProvider>
 	</TRPCProvider>,
 );
